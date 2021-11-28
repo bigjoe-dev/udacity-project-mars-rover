1d54327545795d48728da8819c5ec04d1c6e096c
Joe Delamore <jmgdelamore@gmail.com>
2021-11-28 11:00:51 +0100

    refactor: made tabs function functional

--- src/public/client.js
+++ src/public/client.js
@@ -60,14 +60,13 @@ const Greeting = (name) => {
 const Rovers = (rovers) => {
     let returnHTML = `
     <div class="tab">
-        <button class="tablinks" onclick="openCity(event, 'Curiosity')">Curiosity</button>
-        <button class="tablinks" onclick="openCity(event, 'Perseverance')">Perseverance</button>
-        <button class="tablinks" onclick="openCity(event, 'Spirit')">Spirit</button>
-        <button class="tablinks" onclick="openCity(event, 'Opportunity')">Opportunity</button>
+        ${rovers.reduce((p, c) => {
+            return p += `<button class="tablinks" onclick="updateStore(store, { activeTab: '${c.name}' })">${c.name}</button>`
+        }, '')}
     </div>
     `
     rovers.map((r) => returnHTML += (`
-    <div id="${r.name}" class="tabcontent">
+    <div id="${r.name}" class="${r.name == store.activeTab ? 'tabcontent active' : 'tabcontent'}">
         <p>Latest Images</p> 
         <div class="grid">
             ${r.latest_photos.reduce((p, c) => {
@@ -125,27 +124,4 @@ const getRovers = (state) => {
     fetch(`http://localhost:3000/rovers`)
         .then(res => res.json())
         .then(rovers => updateStore(store, { rovers: rovers.rovers }))
-}
-
-
-
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
 }
